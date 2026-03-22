d99c03c3fbb454afe35378eeaf45bbf62acd8f93
wede-wx <2774016508@qq.com>
2026-03-22 17:54:28 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '32bd872b594c8110ade00002e01ca3c9',
+  rootNotionPageId: '32bf085eaa7e8054932fee1efa98f330',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
